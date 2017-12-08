f58b9b6e71ab56255b2cfe11f20e64ef7a7cd90b
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-07 23:46:09 -0600

    chap-2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'karthikrk1'}
+    posts = [
+        {
+            'author': {'username': 'Kaushik'},
+            'body': 'Beautiful day in Hamburg!'
+        },
+        {
+            'author': {'username': 'Vignesh'},
+            'body': 'The food was great!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
