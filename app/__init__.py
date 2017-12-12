3150811d218998fe7a251da3d4b1460b9608f34e
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-12 17:39:33 -0600

    I18N changes (#2)
    * I18N changes
    
    * req

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 
 
 app = Flask(__name__)
@@ -18,12 +18,12 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view='login'
+login.login_message= _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 babel = Babel(app)
 
-from app import routes, models, errors
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -54,6 +54,11 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
     app.logger.info('Flask App startup')
 
+    
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+from app import routes, models, errors
+
--- config.py
+++ config.py
@@ -13,4 +13,5 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['mailkarthikramki@gmail.com']
     POSTS_PER_PAGE = 25
-    LANGUAGES = ['en', 'es']
+    LANGUAGES=['en', 'es']
+
